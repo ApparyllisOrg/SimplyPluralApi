6654dc7e83b11d517fc64613afdbcafc54d73bc9
Celeste <celeste@saltypandastudios.com>
2022-02-23 08:36:32 +0100

    Check keys length as well prior to updating sp member from pk

--- src/modules/integrations/pk/sync.ts
+++ src/modules/integrations/pk/sync.ts
@@ -155,7 +155,7 @@ export const syncMemberFromPk = async (options: syncOptions, pkMemberId: string,
 			if (options.color || forceSyncProperties) memberDataToSync.color = pkMemberResult.data.color;
 
 			if (spMemberResult) {
-				if (memberDataToSync) {
+				if (memberDataToSync && Object.keys(memberDataToSync).length > 0) {
 					await getCollection("members").updateOne({ uid: userId, pkId: pkMemberId }, { $set: memberDataToSync })
 				}
 				return { success: true, msg: "Member updated on Simply Plural" }
