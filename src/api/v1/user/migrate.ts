a1800c686082141fe7c6f37806b6dc09706d23ab
Cellaryllis <celeste@saltypandastudios.com>
2022-07-02 13:23:34 +0200

    Fix atomic operator

--- src/api/v1/user/migrate.ts
+++ src/api/v1/user/migrate.ts
@@ -3,6 +3,6 @@ import { logger, userLog } from "../../../modules/logger";
 import { getCollection } from "../../../modules/mongo";
 
 export const createUser = async (uid: string) => {
-	await getCollection("private").updateOne({ _id: uid, uid: uid }, { uid: uid }, { upsert: true });
-	await getCollection("users").updateOne({ _id: uid, uid: uid }, { uid: uid }, { upsert: true });
+	await getCollection("private").updateOne({ _id: uid, uid: uid }, { $set:{ uid: uid } }, { upsert: true });
+	await getCollection("users").updateOne({ _id: uid, uid: uid }, { $set:{ uid: uid }}, { upsert: true });
 };
