1363a00cc965d3c8f3f43bd9b186b34255c39245
Celeste <celeste@saltypandastudios.com>
2022-12-25 16:02:10 +0100

    infer daily usage from refreshes

---
+++ src/api/v1/events/open.ts
@@ -0,0 +1,25 @@
+import { getCollection } from "../../../modules/mongo";
+import { getStartOfDay } from "../../../util";
+
+export const logOpenUsage = async (uid: string | undefined) => 
+{
+	if (uid === undefined)
+	{
+		return;
+	}
+
+	const privateUser = await getCollection("private").findOne({uid: uid})
+
+	if (!privateUser)
+	{
+		return
+	}
+
+	let lastRefresh = privateUser.lastRefresh ?? 0;
+
+	if (lastRefresh < getStartOfDay().valueOf())
+	{
+		await getCollection("private").updateOne({uid: uid}, {$set: { lastRefresh }})
+		await getCollection("events").updateOne({date: getStartOfDay(), event: "dailyUsage"}, {$inc: {count : 1}}, { upsert: true })
+	}
+}
