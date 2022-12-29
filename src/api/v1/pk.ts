2af36b164288ca9ac6a2b7fb3e6337e6f8f92e41
Celeste <celeste@saltypandastudios.com>
2022-12-29 09:05:50 +0100

    Guard against empty syncs

--- src/api/v1/pk.ts
+++ src/api/v1/pk.ts
@@ -32,6 +32,21 @@ export const performSyncMemberFromPk = async (req: Request, res: Response) => {
 }
 
 export const performSyncAllMembers = async (req: Request, res: Response) => {
+	const options = req.body.options
+
+	let selectedAnyOption = false;
+	selectedAnyOption = selectedAnyOption || options.name === true;
+	selectedAnyOption = selectedAnyOption || options.avatar === true;
+	selectedAnyOption = selectedAnyOption || options.pronouns === true;
+	selectedAnyOption = selectedAnyOption || options.description === true;
+	selectedAnyOption = selectedAnyOption || options.color === true;
+
+	if (!selectedAnyOption)
+	{
+		res.status(400).send("You need to select at least one data option to sync")
+		return
+	}
+	
 	if (req.query.direction === "push") {
 		performSyncAllMemberToPk(req, res);
 	}
