65d89f73925873e14431240b522c5d569536ecd8
Amaryllis <celeste@saltypandastudios.com>
2023-09-21 17:37:45 +0200

    Add backward support for POST/PATCH with frames

--- src/api/types/frameType.ts
+++ src/api/types/frameType.ts
@@ -1,12 +1,22 @@
 export const frameType = {
     type: "object",
-    properties: {
-        bgShape: { type: "string", pattern: "^[A-Za-z0-9]{0,50}$" },
-        bgClip: { type: "string", pattern: "^[A-Za-z0-9]{0,50}$" },
-        bgStartColor: { type: "string" },
-        bgEndColor: { type: "string" },
-    },
-    nullable: false,
-    additionalProperties: false,
-    required: ["bgShape", "bgClip", "bgStartColor", "bgEndColor"],
+    anyOf: [
+        {
+            properties: {
+                bgShape: { type: ["string", "null"], pattern: "^[A-Za-z0-9]{0,50}$" },
+                bgClip: { type: ["string", "null"], pattern: "^[A-Za-z0-9]{0,50}$" },
+                bgStartColor: { type: ["string", "null"] },
+                bgEndColor: { type: ["string", "null"] },
+            },
+            additionalProperties: false,
+            required: ["bgShape", "bgClip", "bgStartColor", "bgEndColor"],
+        },
+        {
+            properties: {
+            },
+            additionalProperties: false,
+            required: [],
+        }
+    ]
+
 };
