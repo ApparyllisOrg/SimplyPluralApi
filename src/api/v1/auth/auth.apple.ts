d4beae1ceb90083fdc01777b9a1e3be828e251a5
Amaryllis <celeste@saltypandastudios.com>
2024-08-11 11:20:26 +0200

    Add version as optional parameter when registering

--- src/api/v1/auth/auth.apple.ts
+++ src/api/v1/auth/auth.apple.ts
@@ -1,86 +1,93 @@
-import { getCollection } from "../../../modules/mongo";
-import * as Sentry from "@sentry/node";
-import { auth } from "firebase-admin";
-import { getEmailRegex, getNewUid } from "./auth.core";
-import { decode, JwtPayload, verify } from "jsonwebtoken";
-import jwksClient from "jwks-rsa";
-import { migrateAccountFromFirebase } from "./auth.migrate";
-import { setupNewUser } from "../user";
+import { getCollection } from "../../../modules/mongo"
+import * as Sentry from "@sentry/node"
+import { auth } from "firebase-admin"
+import { getEmailRegex, getNewUid } from "./auth.core"
+import { decode, JwtPayload, verify } from "jsonwebtoken"
+import jwksClient from "jwks-rsa"
+import { migrateAccountFromFirebase } from "./auth.migrate"
+import { setupNewUser } from "../user"
 
 async function key(kid: any) {
 	const client = jwksClient({
 		jwksUri: "https://appleid.apple.com/auth/keys",
 		timeout: 30000,
-	});
+	})
 
-	return await client.getSigningKey(kid);
+	return await client.getSigningKey(kid)
 }
 
-export const loginWithApple = async (credential: string): Promise<{ success: boolean; uid: string; email: string }> => {
-	const decoded = decode(credential, { complete: true });
+export const loginWithApple = async (credential: string, version: number | null): Promise<{ success: boolean; uid: string; email: string }> => {
+	const decoded = decode(credential, { complete: true })
 
 	if (!decoded) {
-		return { success: false, uid: "", email: "" };
+		return { success: false, uid: "", email: "" }
 	}
 
-	const kid = decoded.header.kid;
-	const publicKey = (await key(kid)).getPublicKey();
-	const payload: JwtPayload | string = verify(credential, publicKey);
+	const kid = decoded.header.kid
+	const publicKey = (await key(kid)).getPublicKey()
+	const payload: JwtPayload | string = verify(credential, publicKey)
 
 	if (!payload) {
-		return { success: false, uid: "", email: "" };
+		return { success: false, uid: "", email: "" }
 	}
 
 	if (typeof payload === "string") {
-		return { success: false, uid: "", email: "" };
+		return { success: false, uid: "", email: "" }
 	}
 
 	if (payload.aud !== "com.apparyllis.simplyplural" && payload.aud !== "com.apparyllis.simplyplural.web") {
-		return { success: false, uid: "", email: "" };
+		return { success: false, uid: "", email: "" }
 	}
 
-	const email = payload["email"];
-	const account = await getCollection("accounts").findOne({ email : getEmailRegex(payload.email ?? "") });
+	const email = payload["email"]
+	const account = await getCollection("accounts").findOne({ email: getEmailRegex(payload.email ?? "") })
 
 	if (!account) {
-		const result = await registerSub(payload);
+		const result = await registerSub(payload, version)
 
 		if (result !== true) {
-			return { success: false, uid: "", email: "" };
+			return { success: false, uid: "", email: "" }
 		}
 
-		const registeredAccount = await getCollection("accounts").findOne({ email : getEmailRegex(payload.email ?? "") });
+		const registeredAccount = await getCollection("accounts").findOne({ email: getEmailRegex(payload.email ?? "") })
 
 		if (!registeredAccount) {
-			Sentry.captureMessage("Unable to register account of email " + email);
-			return { success: false, uid: "", email: "" };
+			Sentry.captureMessage("Unable to register account of email " + email)
+			return { success: false, uid: "", email: "" }
 		}
 
-		return { success: true, uid: registeredAccount.uid, email: registeredAccount.email };
+		return { success: true, uid: registeredAccount.uid, email: registeredAccount.email }
 	} else {
-		return { success: true, uid: account.uid, email: account.email };
+		return { success: true, uid: account.uid, email: account.email }
 	}
-};
+}
 
-const registerSub = async (payload: JwtPayload): Promise<boolean> => {
+const registerSub = async (payload: JwtPayload, version: number | null): Promise<boolean> => {
 	const firebaseUser = await auth()
 		.getUserByEmail(payload.email ?? "")
-		.catch(() => undefined);
+		.catch(() => undefined)
 	if (!firebaseUser) {
-		const newUserId = await getNewUid();
-		await setupNewUser(newUserId)
-		await getCollection("accounts").insertOne({ uid: newUserId, sub: payload.sub, email: payload.email, verified: true, oAuth2: true, registeredAt: new Date() });
-		return true;
+		const newUserId = await getNewUid()
+		await setupNewUser(newUserId, version)
+		await getCollection("accounts").insertOne({ uid: newUserId, sub: payload.sub, email: payload.email, verified: true, oAuth2: true, registeredAt: new Date() })
+		return true
 	}
 
-	migrateAccountFromFirebase(firebaseUser.uid);
+	migrateAccountFromFirebase(firebaseUser.uid)
 
-	const account = await getCollection("accounts").findOne({ uid: firebaseUser.uid });
+	const account = await getCollection("accounts").findOne({ uid: firebaseUser.uid })
 	if (account) {
-		await getCollection("accounts").updateOne({ uid: firebaseUser.uid }, { $set: { sub: payload.sub } });
+		await getCollection("accounts").updateOne({ uid: firebaseUser.uid }, { $set: { sub: payload.sub } })
 	} else {
-		await getCollection("accounts").insertOne({ uid: firebaseUser.uid, sub: payload.sub, email: firebaseUser.email, verified: true, oAuth2: true, registeredAt: firebaseUser.metadata.creationTime ?? new Date() });
+		await getCollection("accounts").insertOne({
+			uid: firebaseUser.uid,
+			sub: payload.sub,
+			email: firebaseUser.email,
+			verified: true,
+			oAuth2: true,
+			registeredAt: firebaseUser.metadata.creationTime ?? new Date(),
+		})
 	}
 
-	return true;
-};
+	return true
+}
--- src/api/v1/user/migrate.ts
+++ src/api/v1/user/migrate.ts
@@ -1,6 +1,11 @@
 import { getCollection } from "../../../modules/mongo"
 
-export const createUser = async (uid: string) => {
-	await getCollection("private").updateOne({ _id: uid, uid: uid }, { $set: { uid: uid } }, { upsert: true })
+export const createUser = async (uid: string, latestVersion: number | null) => {
+	if (latestVersion === null) {
+		await getCollection("private").updateOne({ _id: uid, uid: uid }, { $set: { uid: uid } }, { upsert: true })
+	} else {
+		await getCollection("private").updateOne({ _id: uid, uid: uid }, { $set: { uid: uid, latestVersion } }, { upsert: true })
+	}
+
 	await getCollection("users").updateOne({ _id: uid, uid: uid }, { $set: { uid: uid } }, { upsert: true })
 }
