df54819de43c3177f79c046302da7e01f0dd1eb9
Amaryllis <celeste@saltypandastudios.com>
2024-08-15 17:40:46 +0200

    Utilize aggregation to fetch custom fields & add pre-flighted fetch collection for bucket access

--- src/api/v1/customFields.ts
+++ src/api/v1/customFields.ts
@@ -1,116 +1,115 @@
-import assert from "assert";
-import { getCollection, parseId } from "../../modules/mongo";
-import { fetchSimpleDocument, fetchCollection, addSimpleDocument, updateSimpleDocument, deleteSimpleDocument } from "../../util";
-import { Request, Response } from "express";
-import { ajv, validateSchema } from "../../util/validation";
-import { ObjectId } from "mongodb";
-import { DiffProcessor } from "../../util/diff";
-import { Diff } from "deep-diff";
-import { insertDefaultPrivacyBuckets } from "./privacy/privacy.assign.defaults";
-import { canSeeMembers } from "../../security";
+import assert from "assert"
+import { getCollection, parseId } from "../../modules/mongo"
+import { fetchSimpleDocument, fetchCollection, addSimpleDocument, updateSimpleDocument, deleteSimpleDocument, fetchBucketsForFriend, fetchCollectionPermissionsPreflighted } from "../../util"
+import { Request, Response } from "express"
+import { ajv, validateSchema } from "../../util/validation"
+import { ObjectId } from "mongodb"
+import { DiffProcessor } from "../../util/diff"
+import { Diff } from "deep-diff"
+import { insertDefaultPrivacyBuckets } from "./privacy/privacy.assign.defaults"
+import { canSeeMembers } from "../../security"
+import { doesUserHaveVersion, FIELD_MIGRATION_VERSION } from "./user/updates/updateUser"
 
 export const NewFieldsVersion = 300
 
-export interface CustomFieldType 
-{
+export interface CustomFieldType {
 	_id: string | ObjectId
-	name: string, 
-	order: number, 
-	privacyBuckets: ObjectId[], 
-	type: number 
+	name: string
+	order: number
+	privacyBuckets: ObjectId[]
+	type: number
 }
 
 export const getCustomField = async (req: Request, res: Response) => {
-	fetchSimpleDocument(req, res, "customFields");
-};
+	fetchSimpleDocument(req, res, "customFields")
+}
 
 export const getCustomFields = async (req: Request, res: Response) => {
-
 	if (req.params.system != res.locals.uid) {
-		const canSee = await canSeeMembers(req.params.system, res.locals.uid);
+		const canSee = await canSeeMembers(req.params.system, res.locals.uid)
 		if (!canSee) {
-			res.status(403).send("You are not authorized to see content of this user");
-			return;
+			res.status(403).send("You are not authorized to see content of this user")
+			return
 		}
-	}
 
-	if (!req.query.sortBy && !req.query.sortOrder)
-	{
-		req.query.sortBy = "order"
-		req.query.sortOrder = "1"
+		const userMigrated = await doesUserHaveVersion(req.params.system, FIELD_MIGRATION_VERSION)
+		if (userMigrated) {
+			const friendBuckets = await fetchBucketsForFriend(res.locals.uid, req.params.system)
+			fetchCollectionPermissionsPreflighted(req, res, "customFields", { buckets: { $in: friendBuckets } })
+		} else {
+			fetchCollection(req, res, "customFields", {})
+		}
+	} else {
+		if (!req.query.sortBy && !req.query.sortOrder) {
+			req.query.sortBy = "order"
+			req.query.sortOrder = "1"
+		}
+		fetchCollection(req, res, "customFields", {})
 	}
-	fetchCollection(req, res, "customFields", {});
-};
+}
 
 export const addCustomField = async (req: Request, res: Response) => {
-
-	const insertBuckets = async (data: any) : Promise<void> =>
-	{
-		await insertDefaultPrivacyBuckets(res.locals.uid, data, 'customFields')
+	const insertBuckets = async (data: any): Promise<void> => {
+		await insertDefaultPrivacyBuckets(res.locals.uid, data, "customFields")
 	}
 
-	await addSimpleDocument(req, res, "customFields", insertBuckets);
-};
+	await addSimpleDocument(req, res, "customFields", insertBuckets)
+}
 
-export const updateCustomField= async (req: Request, res: Response) => {
-
-	const differenceProcessor : DiffProcessor = async (uid: string, difference: Diff<any, any>, lhs: any, rhs: any) =>
-	{
-		if (difference.path![0] === "order")
-		{
+export const updateCustomField = async (req: Request, res: Response) => {
+	const differenceProcessor: DiffProcessor = async (uid: string, difference: Diff<any, any>, lhs: any, rhs: any) => {
+		if (difference.path![0] === "order") {
 			return { processed: true, result: undefined, ignore: true }
 		}
-
 
 		return { processed: false, result: undefined }
 	}
 
-	updateSimpleDocument(req, res, "customFields", differenceProcessor);
-};
+	updateSimpleDocument(req, res, "customFields", differenceProcessor)
+}
 
 export const deleteCustomField = async (req: Request, res: Response) => {
-
-    assert(req.params.id);
+	assert(req.params.id)
 
 	const fieldName = `info.${req.params.id}`
 
-	await getCollection("members").updateMany({ uid: res.locals.uid }, { $unset : {[fieldName]: ''}});
+	await getCollection("members").updateMany({ uid: res.locals.uid }, { $unset: { [fieldName]: "" } })
 
-	deleteSimpleDocument(req, res, "customFields");
-};
+	deleteSimpleDocument(req, res, "customFields")
+}
 
 const s_validatePostCustomFieldSchema = {
 	type: "object",
 	properties: {
 		name: { type: "string" },
 		type: { type: "number" },
-		order: { type: "string", pattern: "^0\|[a-z0-9]{6,}:[a-z0-9]{0,}$" },
+		order: { type: "string", pattern: "^0|[a-z0-9]{6,}:[a-z0-9]{0,}$" },
 		supportMarkdown: { type: "boolean" },
 	},
 	required: ["name", "supportMarkdown", "type", "order"],
 	nullable: false,
-	additionalProperties: false,	
-};
+	additionalProperties: false,
+}
 const v_validatePostCustomFieldSchema = ajv.compile(s_validatePostCustomFieldSchema)
 
 export const validatePostCustomFieldSchema = (body: unknown): { success: boolean; msg: string } => {
-	return validateSchema(v_validatePostCustomFieldSchema, body);
-};
+	return validateSchema(v_validatePostCustomFieldSchema, body)
+}
 
 const s_validatePatchCustomFieldSchema = {
 	type: "object",
 	properties: {
 		name: { type: "string" },
 		type: { type: "number" },
-		order: { type: "string", pattern: "^0\|[a-z0-9]{6,}:[a-z0-9]{0,}$" },
+		order: { type: "string", pattern: "^0|[a-z0-9]{6,}:[a-z0-9]{0,}$" },
 		supportMarkdown: { type: "boolean" },
 	},
 	required: [],
 	nullable: false,
-	additionalProperties: false,	
-};
+	additionalProperties: false,
+}
 const v_validatePatchCustomFieldSchema = ajv.compile(s_validatePatchCustomFieldSchema)
 
 export const validatePatchCustomFieldSchema = (body: unknown): { success: boolean; msg: string } => {
-	return validateSchema(v_validatePatchCustomFieldSchema, body);
-};
+	return validateSchema(v_validatePatchCustomFieldSchema, body)
+}
--- src/api/v1/group.ts
+++ src/api/v1/group.ts
@@ -1,154 +1,165 @@
-import { Request, Response } from "express";
-import { getCollection, parseId } from "../../modules/mongo";
-import { dispatchDelete, OperationType } from "../../modules/socket";
-import { fetchSimpleDocument, addSimpleDocument, updateSimpleDocument, fetchCollection, isMemberOrCustomFront, isMember } from "../../util";
-import { ajv, getPrivacyDependency, validateSchema } from "../../util/validation";
-import { insertDefaultPrivacyBuckets } from "./privacy/privacy.assign.defaults";
-import { canSeeMembers } from "../../security";
+import { Request, Response } from "express"
+import { getCollection, parseId } from "../../modules/mongo"
+import { dispatchDelete, OperationType } from "../../modules/socket"
+import {
+	fetchSimpleDocument,
+	addSimpleDocument,
+	updateSimpleDocument,
+	fetchCollection,
+	isMemberOrCustomFront,
+	isMember,
+	fetchBucketsForFriend,
+	fetchCollectionPermissionsPreflighted,
+} from "../../util"
+import { ajv, getPrivacyDependency, validateSchema } from "../../util/validation"
+import { insertDefaultPrivacyBuckets } from "./privacy/privacy.assign.defaults"
+import { canSeeMembers } from "../../security"
+import { FIELD_MIGRATION_VERSION, doesUserHaveVersion } from "./user/updates/updateUser"
 
 export const getGroups = async (req: Request, res: Response) => {
 	if (req.params.system != res.locals.uid) {
-		const canSee = await canSeeMembers(req.params.system, res.locals.uid);
+		const canSee = await canSeeMembers(req.params.system, res.locals.uid)
 		if (!canSee) {
-			res.status(403).send("You are not authorized to see content of this user");
-			return;
+			res.status(403).send("You are not authorized to see content of this user")
+			return
 		}
-	}
 
-	fetchCollection(req, res, "groups", {});
-};
+		const userMigrated = await doesUserHaveVersion(req.params.system, FIELD_MIGRATION_VERSION)
+		if (userMigrated) {
+			const friendBuckets = await fetchBucketsForFriend(res.locals.uid, req.params.system)
+			fetchCollectionPermissionsPreflighted(req, res, "groups", { buckets: { $in: friendBuckets } })
+		} else {
+			fetchCollection(req, res, "groups", {})
+		}
+	} else {
+		fetchCollection(req, res, "groups", {})
+	}
+}
 
 export const get = async (req: Request, res: Response) => {
-
 	if (req.params.system != res.locals.uid) {
-		const canSee = await canSeeMembers(req.params.system, res.locals.uid);
+		const canSee = await canSeeMembers(req.params.system, res.locals.uid)
 		if (!canSee) {
-			res.status(403).send("You are not authorized to see content of this user");
-			return;
+			res.status(403).send("You are not authorized to see content of this user")
+			return
 		}
 	}
 
-	fetchSimpleDocument(req, res, "groups");
-};
+	fetchSimpleDocument(req, res, "groups")
+}
 
 export const add = async (req: Request, res: Response) => {
-	const insertBuckets = async (data: any) : Promise<void> =>
-	{
-		await insertDefaultPrivacyBuckets(res.locals.uid, data, 'groups')
+	const insertBuckets = async (data: any): Promise<void> => {
+		await insertDefaultPrivacyBuckets(res.locals.uid, data, "groups")
 	}
 
-	addSimpleDocument(req, res, "groups", insertBuckets);
-};
+	addSimpleDocument(req, res, "groups", insertBuckets)
+}
 
 export const setMemberInGroups = async (req: Request, res: Response) => {
-	const member = req.body.member;
-	const isAMember = await isMember(res.locals.uid, member);
+	const member = req.body.member
+	const isAMember = await isMember(res.locals.uid, member)
 	if (!isAMember) {
-		res.status(404).send("Member does not exist");
-		return;
+		res.status(404).send("Member does not exist")
+		return
 	}
 
-	const desiredGroups: string[] = req.body.groups;
+	const desiredGroups: string[] = req.body.groups
 
-	const groups = await getCollection("groups").find({ uid: res.locals.uid });
+	const groups = await getCollection("groups").find({ uid: res.locals.uid })
 
 	await groups.forEach((document) => {
-		let members: string[] | undefined = document.members;
+		let members: string[] | undefined = document.members
 
 		if (members === undefined) {
-			members = [];
+			members = []
 		}
 		if (members !== undefined) {
 			const includesMember = members.includes(member)
 
-			let wantsToIncludeMember = false;
+			let wantsToIncludeMember = false
 			for (let i = 0; i < desiredGroups.length; ++i) {
-				const docId = parseId(document._id);
-				const groupId = parseId(desiredGroups[i]);
+				const docId = parseId(document._id)
+				const groupId = parseId(desiredGroups[i])
 
 				if (docId.toString() === groupId.toString()) {
-					wantsToIncludeMember = true;
+					wantsToIncludeMember = true
 				}
 			}
 
 			if (wantsToIncludeMember !== includesMember) {
-
 				if (wantsToIncludeMember) {
-					getCollection("groups").updateOne({ uid: res.locals.uid, _id: parseId(document._id) }, { $push: { members: member } });
-				}
-				else {
-					getCollection("groups").updateOne({ uid: res.locals.uid, _id: parseId(document._id) }, { $pull: { members: member } });
+					getCollection("groups").updateOne({ uid: res.locals.uid, _id: parseId(document._id) }, { $push: { members: member } })
+				} else {
+					getCollection("groups").updateOne({ uid: res.locals.uid, _id: parseId(document._id) }, { $pull: { members: member } })
 				}
 			}
 		}
 	})
 
-	res.status(200).send();
-};
+	res.status(200).send()
+}
 
 export const update = async (req: Request, res: Response) => {
-	const group = await getCollection("groups").findOne({ uid: res.locals.uid, _id: parseId(req.params.id) });
+	const group = await getCollection("groups").findOne({ uid: res.locals.uid, _id: parseId(req.params.id) })
 
 	if (group) {
 		// eslint-disable-next-line sonarjs/no-collapsible-if
 		if (req.body.private === true && req.body.preventTrusted !== null) {
 			let shouldRecursePrivacyUpdate = false
-			if (!group.private && req.body.private === true)
-			{
+			if (!group.private && req.body.private === true) {
 				shouldRecursePrivacyUpdate = true
 			}
-			if ((group.private && !group.preventTrusted) && req.body.preventTrusted)
-			{
+			if (group.private && !group.preventTrusted && req.body.preventTrusted) {
 				shouldRecursePrivacyUpdate = true
 			}
 			if (shouldRecursePrivacyUpdate) {
-				privateGroupRecursive(req.params.id, res.locals.uid, req.body.private, req.body.preventTrusted);
+				privateGroupRecursive(req.params.id, res.locals.uid, req.body.private, req.body.preventTrusted)
 			}
 		}
 
-		updateSimpleDocument(req, res, "groups");
+		updateSimpleDocument(req, res, "groups")
 	} else {
-		res.status(404).send("Cannot find specified group");
+		res.status(404).send("Cannot find specified group")
 	}
-};
+}
 
 export const del = async (req: Request, res: Response) => {
 	const originalDocument = await getCollection("groups").findOne({ uid: res.locals.uid, _id: parseId(req.params.id) })
-	if (!originalDocument)
-	{
+	if (!originalDocument) {
 		res.status(404).send()
 		return
 	}
 
-	await delGroupRecursive(req.params.id, res.locals.uid);
+	await delGroupRecursive(req.params.id, res.locals.uid)
 
 	//logDeleteAudit(res.locals.uid, "groups", res.locals.operationTime, originalDocument)
 
-	res.status(200).send();
-};
+	res.status(200).send()
+}
 
 const delGroupRecursive = async (groupId: string, uid: string) => {
-	const groups = await getCollection("groups").find({ uid, parent: groupId }).toArray();
+	const groups = await getCollection("groups").find({ uid, parent: groupId }).toArray()
 	for (let i = 0; i < groups.length; i++) {
-		await delGroupRecursive(groups[i]._id.toString(), uid);
+		await delGroupRecursive(groups[i]._id.toString(), uid)
 	}
-	await getCollection("groups").deleteOne({ uid, _id: parseId(groupId) });
+	await getCollection("groups").deleteOne({ uid, _id: parseId(groupId) })
 	dispatchDelete({
 		operationType: OperationType.Delete,
 		uid,
 		documentId: groupId,
 		collection: "groups",
-	});
-};
+	})
+}
 
 const privateGroupRecursive = async (groupId: string, uid: string, priv: boolean, preventTrusted: boolean) => {
-	const groups = await getCollection("groups").find({ uid, parent: groupId }).toArray();
+	const groups = await getCollection("groups").find({ uid, parent: groupId }).toArray()
 	for (let i = 0; i < groups.length; i++) {
-		await privateGroupRecursive(groups[i]._id.toString(), uid, priv, preventTrusted);
+		await privateGroupRecursive(groups[i]._id.toString(), uid, priv, preventTrusted)
 	}
 
-	await getCollection("groups").updateOne({ uid, _id: parseId(groupId) }, { $set: { private: priv, preventTrusted } });
-};
+	await getCollection("groups").updateOne({ uid, _id: parseId(groupId) }, { $set: { private: priv, preventTrusted } })
+}
 
 const s_validateGroupSchema = {
 	type: "object",
@@ -166,12 +177,12 @@ const s_validateGroupSchema = {
 	nullable: false,
 	additionalProperties: false,
 	dependencies: getPrivacyDependency(),
-};
+}
 const v_validateGroupSchema = ajv.compile(s_validateGroupSchema)
 
 export const validateGroupSchema = (body: unknown): { success: boolean; msg: string } => {
-	return validateSchema(v_validateGroupSchema, body);
-};
+	return validateSchema(v_validateGroupSchema, body)
+}
 
 const s_validateSetMemberInGroupSchema = {
 	type: "object",
@@ -182,12 +193,12 @@ const s_validateSetMemberInGroupSchema = {
 	nullable: false,
 	additionalProperties: false,
 	required: ["member", "groups"],
-};
+}
 const v_validateSetMemberInGroupSchema = ajv.compile(s_validateSetMemberInGroupSchema)
 
 export const validateSetMemberInGroupSchema = (body: unknown): { success: boolean; msg: string } => {
-	return validateSchema(v_validateSetMemberInGroupSchema, body);
-};
+	return validateSchema(v_validateSetMemberInGroupSchema, body)
+}
 
 const s_validatePostGroupSchema = {
 	type: "object",
@@ -206,9 +217,9 @@ const s_validatePostGroupSchema = {
 	nullable: false,
 	additionalProperties: false,
 	dependencies: getPrivacyDependency(),
-};
+}
 const v_validatePostGroupSchema = ajv.compile(s_validatePostGroupSchema)
 
 export const validatePostGroupSchema = (body: unknown): { success: boolean; msg: string } => {
-	return validateSchema(v_validatePostGroupSchema, body);
-};
+	return validateSchema(v_validatePostGroupSchema, body)
+}
