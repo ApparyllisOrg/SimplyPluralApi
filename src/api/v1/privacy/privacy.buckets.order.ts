92137b68abd82fa21a26c25b7052aca324ffb241
Amaryllis <celeste@saltypandastudios.com>
2024-09-06 12:09:34 +0200

    Add orders endpoint for fields

--- src/api/v1/privacy/privacy.buckets.order.ts
+++ src/api/v1/privacy/privacy.buckets.order.ts
@@ -1,38 +1,40 @@
-import { Request, Response } from "express";
-import { getCollection, parseId } from "../../../modules/mongo";
-import { ajv, validateSchema } from "../../../util/validation";
+import { Request, Response } from "express"
+import { getCollection, parseId } from "../../../modules/mongo"
+import { ajv, validateSchema } from "../../../util/validation"
 
 export const orderBuckets = async (req: Request, res: Response) => {
-    const buckets : {id: string, rank: string}[] = req.body.buckets
-   
-    for (let i = 0; i < buckets.length; ++i)
-    {
-        getCollection("privacyBuckets").updateOne({ uid: res.locals.uid, _id: parseId(buckets[i].id) }, { $set: { rank: buckets[i].rank }});
-    }
+	const buckets: { id: string; rank: string }[] = req.body.buckets
+
+	for (let i = 0; i < buckets.length; ++i) {
+		getCollection("privacyBuckets").updateOne({ uid: res.locals.uid, _id: parseId(buckets[i].id) }, { $set: { rank: buckets[i].rank } })
+	}
 
 	res.status(200).send()
-};
+}
 
 const s_validateOrderBucketsSchema = {
-    type: "object", 
-    properties: {
-        buckets: { type: "array",  items: {
-            type: "object",
-            properties: {
-                id: { type: "string", pattern: "^[A-Za-z0-9]{20,50}$" },
-                rank: { type: "string", pattern: "^0\|[a-z0-9]{6,}:[a-z0-9]{0,}$" },
-            },
-            required: ["id", "rank",],
-            nullable: false,
-            additionalProperties: false,
-        }}
-    },
-    required: ["buckets",],
-    nullable: false,
-    additionalProperties: false,
-};
+	type: "object",
+	properties: {
+		buckets: {
+			type: "array",
+			items: {
+				type: "object",
+				properties: {
+					id: { type: "string", pattern: "^[A-Za-z0-9]{20,50}$" },
+					rank: { type: "string", pattern: "^0|[a-z0-9]{6,}:[a-z0-9]{0,}$" },
+				},
+				required: ["id", "rank"],
+				nullable: false,
+				additionalProperties: false,
+			},
+		},
+	},
+	required: ["buckets"],
+	nullable: false,
+	additionalProperties: false,
+}
 const v_validateOrderBucketsSchema = ajv.compile(s_validateOrderBucketsSchema)
 
 export const validateOrderBucketsSchema = (body: unknown): { success: boolean; msg: string } => {
-	return validateSchema(v_validateOrderBucketsSchema, body);
-};
+	return validateSchema(v_validateOrderBucketsSchema, body)
+}
